9dfe87b599febad32baef0a79f3c7deb90a36353
Paweł <pawel.mstowski@gmail.com>
2019-03-14 20:05:16 +0100

    Add pull request creator

---
+++ git_repo_master/azure_devops/pull_request_creator.py
@@ -0,0 +1,28 @@
+from git_repo_master.azure_devops.request import AzureGitRequest
+
+
+class PullRequestCreator:
+    def __init__(self, organization, project, token):
+        self.__request = AzureGitRequest() \
+            .with_organization(organization) \
+            .with_project(project) \
+            .with_resource('pullrequests') \
+            .with_personaltoken(token) \
+            .with_method('POST')
+
+    def create_pull_request(self,
+                            repository,
+                            source_branch,
+                            target_branch,
+                            title,
+                            description):
+        self.__request \
+            .with_repository(repository) \
+            .with_body({
+                "sourceRefName": "refs/heads/{}".format(source_branch),
+                "targetRefName": "refs/heads/{}".format(target_branch),
+                "title": title,
+                "description": description,
+            }) \
+            .execute()
+
--- grm.py
+++ grm.py
@@ -5,13 +5,17 @@ from git_repo_master.azure_actions import ACTIONS
 
 def startup_parser():
     parser = argparse.ArgumentParser()
-    group = parser.add_argument_group('Required Arguments')
-    group.add_argument('-organization', help='Azure DevOps organization', required=True)
-    group.add_argument('-project', help='Azure DevOps project in given organization', required=True)
-    group.add_argument('-repository', help='Repo for action', required=True, nargs='+')
-    group.add_argument('-token', help='Personal access token for Azure DevOps', required=True)
-    group.add_argument('-branch', help='Branch to perform action on', required=True)
-    group.add_argument('-action', help='Action to perform', required=True, choices=ACTIONS.keys())
+    group = parser.add_argument_group('All arguments')
+    group.add_argument('-organization', help='Azure DevOps organization')
+    group.add_argument('-project', help='Azure DevOps project in given organization')
+    group.add_argument('-repository', help='Repo for action', nargs='+')
+    group.add_argument('-token', help='Personal access token for Azure DevOps')
+    group.add_argument('-branch', help='Branch to perform action on')
+    group.add_argument('-from', help='Branch name from which action has to be done')
+    group.add_argument('-to', help='Branch name to which action has to be done')
+    group.add_argument('-title', help='Pull request title')
+    group.add_argument('-description', help='Pull request description')
+    group.add_argument('-action', help='Action to perform', choices=ACTIONS.keys())
 
     args = parser.parse_args()
     return args
